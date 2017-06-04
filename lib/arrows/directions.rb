bdd542522a9edfa1c79115c611272f979fef9e91
Georgi Kostov <joromir@protonmail.com>
2017-06-04 11:57:29 +0300

    add naive solution for Arrows::Directions

--- lib/arrows/directions.rb
+++ lib/arrows/directions.rb
@@ -1,11 +1,26 @@
 module Arrows
   class Directions
-    attr_reader :sequence
+    attr_reader :sequence, :abscissa, :ordinate
 
     def initialize(sequence)
       @sequence = sequence
+      @abscissa = 0
+      @ordinate = 0
     end
 
     alias to_s sequence
+
+    def to_coordinates
+      sequence.split('').each do |arrow|
+        case arrow
+        when '>' then @abscissa = abscissa + 1
+        when '<' then @abscissa = abscissa - 1
+        when '^' then @ordinate = ordinate + 1
+        when 'v' then @ordinate = ordinate - 1
+        end
+      end
+
+      [abscissa, ordinate]
+    end
   end
 end
--- spec/arrows/directions_spec.rb
+++ spec/arrows/directions_spec.rb
@@ -22,4 +22,62 @@ RSpec.describe Arrows::Directions do
       end
     end
   end
+
+  describe '#to_coordinates' do
+    let(:sequence) { '' }
+
+    subject { described_class.new(sequence) }
+
+    it 'responds to to_coordinates' do
+      expect(subject).to respond_to(:to_coordinates)
+    end
+
+    it 'holds the value of 0 for ordinate initially' do
+      expect(subject.ordinate).to eq 0
+    end
+
+    it 'holds the value of 0 for abscissa initially' do
+      expect(subject.abscissa).to eq 0
+    end
+
+    context 'when sequence is ">"' do
+      let(:sequence) { '>' }
+
+      it 'evaluates to [1, 0]' do
+        expect(subject.to_coordinates).to eq [1, 0]
+      end
+    end
+
+    context 'when sequence is "<"' do
+      let(:sequence) { '<' }
+
+      it 'evaluates to [-1, 0]' do
+        expect(subject.to_coordinates).to eq [-1, 0]
+      end
+    end
+
+    context 'when sequence is "^"' do
+      let(:sequence) { '^' }
+
+      it 'evaluates to [0, 1]' do
+        expect(subject.to_coordinates).to eq [0, 1]
+      end
+    end
+
+    context 'when sequence is "v"' do
+      let(:sequence) { 'v' }
+
+      it 'evaluates to [0, -1]' do
+        expect(subject.to_coordinates).to eq [0, -1]
+      end
+    end
+
+    context 'when sequence is ""' do
+      let(:sequence) { '' }
+
+      it 'evaluates to [0, 0]' do
+        expect(subject.to_coordinates).to eq [0, 0]
+      end
+    end
+  end
 end
